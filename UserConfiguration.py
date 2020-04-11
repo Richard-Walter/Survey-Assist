cace893302b03d5e1417ace5bff71add8dcfda2b
Richard Walter <willthisnamedo@gmail.com>
2020-04-11 18:47:09 +1000

    user config creates user directory if doesn't exist

--- UserConfiguration.py
+++ UserConfiguration.py
@@ -1,14 +1,14 @@
 from configparser import ConfigParser
-
+import os
 
 class UserConfiguration:
-
-
     section_file_directories = 'FILE DIRECTORIES'
+    user_settings_directory = r"c:/SurveyAssist"
+
+    if not os.path.isdir(user_settings_directory):
+        os.makedirs(user_settings_directory)
 
     def __init__(self):
-
-        # @ todo create a c:/SurveyAssist/user_settings.ini if one doesnt exist
 
         # read in config file
         self.config_file_path = 'c:/SurveyAssist/user_settings.ini'
@@ -19,9 +19,7 @@ class UserConfiguration:
         self.user_sd_root = self.config_parser.get(UserConfiguration, 'user_sd_root')
 
     def update(self, section, key, value):
-
         self.config_parser.set(section, key, value)
 
         with open(self.config_file_path, 'w+') as f:
             self.config_parser.write(f)
-
