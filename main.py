8d1678170b0f78e7e91e9c20e6d40a3973c5b0ef
Richard Walter <willthisnamedo@gmail.com>
2020-04-24 14:39:53 +1000

    moved job diary

--- main.py
+++ main.py
@@ -70,14 +70,13 @@ class MenuBar(tk.Frame):
         self.file_sub_menu.add_command(label="Create Dated Directory...", command=lambda: self.new_dated_directory(False))
         self.file_sub_menu.add_command(label="Create Job Directory...", command=self.new_job_directoy)
         self.file_sub_menu.add_command(label="Import SD Data", command=self.import_sd_data)
-        self.file_sub_menu.add_command(label="Job Diary", command=self.job_diary)
-
         self.file_sub_menu.add_separator()
         self.file_sub_menu.add_command(label="Monitoring - Create", command=self.monitoring_create, state="disabled")
         self.file_sub_menu.add_command(label="Monitoring - Update Coordinates", command=self.monitoring_update_coords, state="disabled")
         self.file_sub_menu.add_command(label="Monitoring - Update Labels", command=self.monitoring_update_labels, state="disabled")
         self.file_sub_menu.add_command(label="Monitoring - Rename Updated Files", command=self.monitoring_rename_updated_files, state="disabled")
         self.file_sub_menu.add_separator()
+        self.file_sub_menu.add_command(label="Job Diary", command=self.job_diary)
         self.file_sub_menu.add_command(label="Configuration", command=self.configure_survey)
 
         self.menu_bar.add_cascade(label="File", menu=self.file_sub_menu)
